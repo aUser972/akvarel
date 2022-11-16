353d1d1d0accc545c80e2e5f581e198f20a15cca
baitetatet <di.baitov@mail.ru>
2022-11-16 09:33:30 +0300

    [F] fix styles

--- src/Components/Aside/SelectRadius/SelectRadius.jsx
+++ src/Components/Aside/SelectRadius/SelectRadius.jsx
@@ -3,10 +3,11 @@ import { CustomCircle } from "../../CustomCircleContext"
 
 
 export const SelectRadius = () => {
-  const { setCustomCircle } = useContext(CustomCircle)
+  const { customCircle, setCustomCircle } = useContext(CustomCircle)
   const VARIABLES = {
     labelText: 'Введите радиус окружности:',
-    btnText: 'Выбрать область'
+    btnText: 'Выбрать область',
+    btnCancelText: 'Отменить выбор'
   }
   const getRadius = () => {
     const radius = document.querySelector('.aside__select-radius__select_input')
@@ -14,39 +15,46 @@ export const SelectRadius = () => {
   }
   const handlerSelectRadius = (event) => {
     event.preventDefault()
-    setCustomCircle({
-      objectType: '',
-      maxConsumers: '',
-      minConsumers: '',
-      Longtitude: '',
-      Lattitude: '',
-      Radius: getRadius(),
-      numberPosts: '',
-    })
+    if (customCircle) {
+      setCustomCircle(null)
+      const radius = document.querySelector('.aside__select-radius__select_input')
+      radius.valueAsNumber = 0
+    } else {
+      setCustomCircle({
+        objectType: '',
+        maxConsumers: '',
+        minConsumers: '',
+        Longtitude: '',
+        Lattitude: '',
+        Radius: getRadius(),
+        numberPosts: '',
+      })
+    }
   }
 
   return (
     <div className="aside__select-radius">
       <div className="aside__select-radius__inner">
+        <h2 className="aside__select-radius__title">
+          {VARIABLES.labelText}
+        </h2>
         <div className="aside__select-radius__select">
-          <label >
-            {VARIABLES.labelText}
-            <input
-              className="aside__select-radius__select_input"
-              type="number"
-              min="0"
-              placeholder="0 метров"
-              step="10"
-            />
-          </label>
+          <input
+            className="aside__select-radius__select_input"
+            id="select-radius__input"
+            type="number"
+            min="0"
+            placeholder="0 метров"
+            step="10"
+          />
+          <button
+            className="aside__select-radius__select_btn button_green"
+            onClick={(event) => handlerSelectRadius(event)}
+          >
+            {customCircle ? VARIABLES.btnCancelText : VARIABLES.btnText}
+          </button>
         </div>
-        <button
-          className="aside__select-radius_btn button_green"
-          onClick={(event) => handlerSelectRadius(event)}
-        >
-          {VARIABLES.btnText}
-        </button>
       </div>
-    </div>
+    </div >
   )
 }
